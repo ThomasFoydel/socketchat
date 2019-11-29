741baf5c1b1fb194a4d2fa669678f5fccc9674dd
Thomas Foydel <tfoydel@mail.roosevelt.edu>
2019-11-28 18:58:26 -0600

    made image posts bigger and fixed individual image link in private convo to link back to private convo instead of blank

--- client/src/pages/individualimage/IndividualImage.js
+++ client/src/pages/individualimage/IndividualImage.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import { CTX } from 'context/Store';
 
 import './IndividualImage.scss';
 
 const IndividualImage = ({ match }) => {
+  const [appState] = useContext(CTX);
   const { id } = match.params;
+  const { currentPrivateFriend } = appState;
 
   return (
     <div className='individualimage-container'>
-      <Link to={`/chat`}>
+      <Link
+        to={
+          currentPrivateFriend
+            ? `/privateconvo/${currentPrivateFriend}`
+            : `/chat`
+        }
+      >
         <img
           alt='individual close up of message content'
           src={`/messageimage/${id}`}
